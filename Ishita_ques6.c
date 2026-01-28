aef29220bc1413ccf6fbe8ac5b0b8887eb7c6c94
Ishita Jain <216325024+ishitaajain22-tech@users.noreply.github.com>
2026-01-28 16:51:03 +0530

    ques6

---
+++ Ishita_ques6.c
@@ -0,0 +1,12 @@
+#include <iostream>
+#include<math.h>
+using namespace std;
+
+int main() {
+    int a,b;
+    cout<<"Enter num: ";
+    cin>>a>>b;
+    int largest = max(a,b);
+    cout<<largest;
+    return 0;
+}
