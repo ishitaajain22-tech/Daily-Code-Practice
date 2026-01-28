4b394fc8af478626873df0bc3b19618f3db7595b
Ishita Jain <216325024+ishitaajain22-tech@users.noreply.github.com>
2026-01-28 16:28:27 +0530

    ques1

---
+++ Ishita_q1.c
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num = -15;
+        
+    if(num == 0){
+        cout << "Zero";
+    }else (num > 0) ? cout << "Positive": cout << "Negative";
+}
