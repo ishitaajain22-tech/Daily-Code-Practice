c2b479f57023413772833b4c4aa5d50d39b82230
Ishita Jain <216325024+ishitaajain22-tech@users.noreply.github.com>
2026-01-29 09:37:15 +0530

    ques11
    Sum of digits

---
+++ Ishita_q11.c
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int getSum(int num, int sum){
+    if(num==0) return sum;
+    sum+=(num%10);
+    return getSum(num/10,sum);
+}
+
+int main ()
+{
+    int num, sum = 0;
+    cout<<"Enter number: ";
+    cin>>num;
+    cout <<"Sum of digits : " << getSum(num, sum);
+ 
+    return 0;
+}
