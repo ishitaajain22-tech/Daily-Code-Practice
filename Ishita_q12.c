93042ee14c8104889cca85935fb6a77731233ab0
Ishita Jain <216325024+ishitaajain22-tech@users.noreply.github.com>
2026-01-29 09:53:09 +0530

    ques12
    palindrome check of string

---
+++ Ishita_q12.c
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+void lower(char str[]){
+  int i = 0;
+  while (str[i] != '\0'){
+    str[i] = tolower(str[i]);
+    i++;
+  }
+}
+int main() 
+{
+    char str[10] = "Radar";
+    int i, len, flag = 0;
+    
+    lower(str);
+    
+    len = strlen(str);
+    
+    for (i = 0; i < len / 2; i++) 
+    {
+        if (str[i] != str[len - i - 1]){
+            flag++;
+            break;
+        }
+    }
+
+    if (flag)
+        cout << str << " is not palindrome";
+    else
+        cout << str << " is palindrome";
+        
+    return 0;
+}
